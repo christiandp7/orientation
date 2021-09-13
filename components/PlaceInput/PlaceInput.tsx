7da2619dbdb956809bc5fa9204b5e56110da9e98
Christian Diaz Portela <christiandp7@gmail.com>
2021-09-13 17:25:29 -0400

    :building_construction: Setup tailwind font family and colors, Scaffold PlaceInput

---
+++ components/PlaceInput/PlaceInput.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const PlaceInput = () => {
+  return <div>input</div>;
+};
+
+export default PlaceInput;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,24 @@
+const colors = require("tailwindcss/colors");
+
 module.exports = {
-  mode: 'jit',
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  mode: "jit",
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        futura: ['"Futura PT Web Cond Bold"', "sans-serif"],
+      },
+      colors: {
+        primary: colors.black,
+        grayBg: "#F4F4F4",
+        grayBorder: "#8F8E8E",
+        grayText: "#AAAAAA",
+      },
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
