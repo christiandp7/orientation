ac482f612f337eb5f925adb494db68af23b359a3
Christian Diaz Portela <christiandp7@gmail.com>
2021-09-14 08:50:05 -0400

    :ambulance: fix script google maps

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,14 @@
-import React from "react";
-import Footer from "@components/Footer";
-import Layout from "@components/Layout";
-import PlaceInput from "@components/PlaceInput";
+import React from 'react'
+import Footer from '@components/Footer'
+import Layout from '@components/Layout'
+import PlaceInput from '@components/PlaceInput'
 
 const Home = () => {
-  return (
-    <Layout>
-      <PlaceInput />
-    </Layout>
-  );
-};
+	return (
+		<Layout>
+			<PlaceInput />
+		</Layout>
+	)
+}
 
-export default Home;
+export default Home
